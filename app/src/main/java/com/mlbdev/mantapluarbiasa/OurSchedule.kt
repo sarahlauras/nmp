36c014393a5793212f068c814a6ef3efec186398
nadin388 <s160422027@student.ubaya.ac.id>
2024-10-07 21:31:06 +0700

    blm recycler view sm linear layout

--- app/src/main/java/com/mlbdev/mantapluarbiasa/OurSchedule.kt
+++ app/src/main/java/com/mlbdev/mantapluarbiasa/OurSchedule.kt
@@ -18,3 +18,4 @@ class OurSchedule : AppCompatActivity() {
         }
     }
 }
+
---
+++ app/src/main/java/com/mlbdev/mantapluarbiasa/ScheduleArray.kt
@@ -0,0 +1,4 @@
+package com.mlbdev.mantapluarbiasa
+
+data class ScheduleArray(var tanggal:String, var bulan:String, var nama_schedule:String,
+                         var nama_game:String, var nama_team:String)
