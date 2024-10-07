e1ebc8278d3e9c20cffb665d004c2159a5b36bed
sarahlauras <s160422015@student.ubaya.ac.id>
2024-10-07 13:50:58 +0700

    a

---
+++ app/src/main/java/com/mlbdev/mantapluarbiasa/AchievementList.kt
@@ -0,0 +1,20 @@
+package com.mlbdev.mantapluarbiasa
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AchievementList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_achievement_list)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
