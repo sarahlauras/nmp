f9559723b8ef5e1b45af06df9d6a953897db880c
nadin388 <s160422027@student.ubaya.ac.id>
2024-10-09 21:24:37 +0700

    schedule

--- app/src/main/java/com/mlbdev/mantapluarbiasa/OurSchedule.kt
+++ app/src/main/java/com/mlbdev/mantapluarbiasa/OurSchedule.kt
@@ -5,17 +5,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mlbdev.mantapluarbiasa.databinding.ActivityOurScheduleBinding
+import com.mlbdev.mantapluarbiasa.databinding.ScheduleCardBinding
 
 class OurSchedule : AppCompatActivity() {
+    private lateinit var binding:ActivityOurScheduleBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_our_schedule)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityOurScheduleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.schedulePage.layoutManager = LinearLayoutManager(this)
+        binding.schedulePage.setHasFixedSize(true)
+        binding.schedulePage.adapter = QuestionAdapter()
     }
 }
-
