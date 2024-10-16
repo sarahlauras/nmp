0ffcb107a380ff0ef0296535358d24043dcdbafa
sarahlauras <s160422015@student.ubaya.ac.id>
2024-10-16 15:20:31 +0700

    don

--- app/src/main/java/com/mlbdev/mantapluarbiasa/MembersData.kt
+++ app/src/main/java/com/mlbdev/mantapluarbiasa/MembersData.kt
@@ -2,28 +2,28 @@ package com.mlbdev.mantapluarbiasa
 
 object MembersData {
     var memberData = arrayOf(
-        MembersBank(1, 0, 1, "Wyn", "Duelist", ""),
-        MembersBank(2, 0, 1, "Luna", "Sentinel", ""),
-        MembersBank(3, 0, 1, "Kai", "Initiator", ""),
+        MembersBank(1, 0, 1, "Wyn", "Duelist", R.drawable.member1),
+        MembersBank(2, 0, 1, "Luna", "Sentinel", R.drawable.member2),
+        MembersBank(3, 0, 1, "Kai", "Initiator", R.drawable.member3),
 
-        MembersBank(4, 0, 2, "Jax", "Duelist", ""),
-        MembersBank(5, 0, 2, "Mira", "Controller", ""),
-        MembersBank(6, 0, 2, "Zed", "Sentinel", ""),
+        MembersBank(4, 0, 2, "Jax", "Duelist", R.drawable.member4),
+        MembersBank(5, 0, 2, "Mira", "Controller", R.drawable.member5),
+        MembersBank(6, 0, 2, "Zed", "Sentinel", R.drawable.member6),
 
-        MembersBank(7, 1, 3, "Leo", "Marksman", ""),
-        MembersBank(8, 1, 3, "Raya", "Support", ""),
-        MembersBank(9, 1, 3, "Ken", "Tank", ""),
+        MembersBank(7, 1, 3, "Leo", "Marksman", R.drawable.member1),
+        MembersBank(8, 1, 3, "Raya", "Support", R.drawable.member2),
+        MembersBank(9, 1, 3, "Ken", "Tank", R.drawable.member3),
 
-        MembersBank(10, 1, 4, "Fir", "Assassin", ""),
-        MembersBank(11, 1, 4, "Nova", "Mage", ""),
-        MembersBank(12, 1, 4, "Aris", "Fighter", ""),
+        MembersBank(10, 1, 4, "Fir", "Assassin", R.drawable.member4),
+        MembersBank(11, 1, 4, "Nova", "Mage", R.drawable.member5),
+        MembersBank(12, 1, 4, "Aris", "Fighter", R.drawable.member6),
 
-        MembersBank(13, 2, 5, "Sean", "Sniper", ""),
-        MembersBank(14, 2, 5, "Alex", "Scout", ""),
-        MembersBank(15, 2, 5, "Blake", "Medic", ""),
+        MembersBank(13, 2, 5, "Sean", "Sniper", R.drawable.member1),
+        MembersBank(14, 2, 5, "Alex", "Scout", R.drawable.member2),
+        MembersBank(15, 2, 5, "Blake", "Medic", R.drawable.member3),
 
-        MembersBank(16, 2, 6, "Rico", "Fragger", ""),
-        MembersBank(17, 2, 6, "Tara", "Driver", ""),
-        MembersBank(18, 2, 6, "Vince", "Leader", "")
+        MembersBank(16, 2, 6, "Rico", "Fragger", R.drawable.member4),
+        MembersBank(17, 2, 6, "Tara", "Driver", R.drawable.member5),
+        MembersBank(18, 2, 6, "Vince", "Leader", R.drawable.member6)
     )
 }
