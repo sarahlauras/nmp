fb25ae14065866c3993f99b94e4ac130c0070246
sarahlauras <s160422015@student.ubaya.ac.id>
2024-11-14 19:25:54 +0700

    a

--- app/src/main/java/com/mlbdev/mantapluarbiasa/SignUp.kt
+++ app/src/main/java/com/mlbdev/mantapluarbiasa/SignUp.kt
@@ -5,15 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.mlbdev.mantapluarbiasa.databinding.ActivityIntroBinding
+import com.mlbdev.mantapluarbiasa.databinding.ActivitySignUpBinding
 
-class IntroActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityIntroBinding
+class SignUp : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_intro)
+        setContentView(R.layout.activity_sign_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
