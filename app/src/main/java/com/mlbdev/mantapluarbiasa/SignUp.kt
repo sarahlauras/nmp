ca6793f0271179dccff619d9fa12f3c736eed79f
sarahlauras <s160422015@student.ubaya.ac.id>
2024-11-16 18:51:22 +0700

    progress sign in up

--- app/src/main/java/com/mlbdev/mantapluarbiasa/SignUp.kt
+++ app/src/main/java/com/mlbdev/mantapluarbiasa/SignUp.kt
@@ -8,16 +8,29 @@ import androidx.core.view.WindowInsetsCompat
 import com.mlbdev.mantapluarbiasa.databinding.ActivitySignUpBinding
 
 class SignUp : AppCompatActivity() {
-    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var binding:ActivitySignUpBinding
+    val sharedPreferences = getSharedPreferences("UserSession", MODE_PRIVATE)
+    val editor = sharedPreferences.edit()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val username = binding.txtUsername
+            val password = binding.txtPassword
+            signUp(username.toString(), password.toString())
         }
     }
+
+    fun signUp(username: String, password: String) {
+        val hashedPassword = password.hashCode().toString()
+        val editor = sharedPreferences.edit()
+        editor.putString("Username", username)
+        editor.putString("Password", hashedPassword)
+        editor.apply()
+        Toast.makeText(this, "Sign-Up Successful!", Toast.LENGTH_SHORT).show()
+    }
+
 }
