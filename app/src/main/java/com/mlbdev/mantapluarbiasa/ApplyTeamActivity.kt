9ce674501637e4132913a12a235216fc6381e392
sarahlauras <s160422015@student.ubaya.ac.id>
2024-12-02 10:52:14 +0700

    apply team

---
+++ app/src/main/java/com/mlbdev/mantapluarbiasa/ApplyTeamActivity.kt
@@ -0,0 +1,23 @@
+package com.mlbdev.mantapluarbiasa
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mlbdev.mantapluarbiasa.databinding.ActivityApplyTeamBinding
+import com.mlbdev.mantapluarbiasa.databinding.ActivityMemberBinding
+
+class ApplyTeamActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityApplyTeamBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityApplyTeamBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_apply_team)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+}
