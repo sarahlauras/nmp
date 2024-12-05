98b0a0166049bbf055b278f52197e9a0e26438ec
cennialieta <s160422037@student.ubaya.ac.id>
2024-12-05 14:01:05 +0700

    what we play

--- app/src/main/java/com/mlbdev/mantapluarbiasa/WhatWePlayFragment.kt
+++ app/src/main/java/com/mlbdev/mantapluarbiasa/WhatWePlayFragment.kt
@@ -1,15 +1,24 @@
 package com.mlbdev.mantapluarbiasa
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.mlbdev.mantapluarbiasa.databinding.ActivityOurScheduleBinding
 import com.mlbdev.mantapluarbiasa.databinding.ActivityWhatWePlayBinding
 import com.mlbdev.mantapluarbiasa.databinding.FragmentWhatWePlayBinding
 import com.mlbdev.mantapluarbiasa.databinding.FragmentWhoWeAreBinding
+import org.json.JSONObject
 
 private val ARG_GAME = "arraygame"
 
@@ -20,8 +29,17 @@ class WhatWePlayFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            whatwe_play = it.getParcelableArrayList<GameBank>(ARG_GAME) as ArrayList<GameBank>
+//        arguments?.let {
+//            whatwe_play = it.getParcelableArrayList<GameBank>(ARG_GAME) as ArrayList<GameBank>
+//        }
+        val sharedPreferences = requireContext().getSharedPreferences("USER_PREFERENCES", Context.MODE_PRIVATE)
+        val username = sharedPreferences.getString("USERNAME", null)
+
+        if(username != null){
+            readGame(username)
+        }
+        else{
+            Toast.makeText(context, "Username not found", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -46,6 +64,44 @@ class WhatWePlayFragment : Fragment() {
             arguments = Bundle().apply {
                 putParcelableArrayList(ARG_GAME, whatwe_play)
             }
+        }
+    }
+    fun readGame(username : String) {
+        val queue = Volley.newRequestQueue(requireContext())
+        val url = "https://ubaya.xyz/native/160422015/ourschedule.php"
+
+        val stringRequest = object : StringRequest(
+            Request.Method.POST,
+            url,
+            {
+                val obj = JSONObject(it)
+                if (obj.getString("result") == "OK") {
+                    val data = obj.getJSONArray("data")
+                    val sType = object : TypeToken<List<GameBank>>() {}.type
+                    whatwe_play = Gson().fromJson(data.toString(), sType) as ArrayList<GameBank>
+                    updateList()
+                } else {
+                    Toast.makeText(context, "No data found", Toast.LENGTH_SHORT).show()
+                }
+            },
+            {
+                Log.e("apiresult", it.message.toString())
+            }
+        ) {
+            override fun getParams(): MutableMap<String, String> {
+                val params = HashMap<String, String>()
+                params["username"] = username // Tambahkan parameter username
+                return params
+            }
+        }
+        queue.add(stringRequest)
+    }
+    fun updateList(){
+        val lm = LinearLayoutManager(activity)
+        with(binding.recGames){
+            layoutManager = lm
+            setHasFixedSize(true)
+            adapter = GameAdapter(whatwe_play)
         }
     }
 }
